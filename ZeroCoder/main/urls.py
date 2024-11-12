b54e89b5faa6ca80a3601062dd308020c3309f9b
Ivan Zyulin <solar.ivz@gmail.com>
2024-11-12 23:02:08 +1200

    add data and test pages into main app

--- ZeroCoder/main/urls.py
+++ ZeroCoder/main/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('data', views.data),
+    path('test', views.test)
+
 ]
--- ZeroCoder/main/views.py
+++ ZeroCoder/main/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>Это мой первый проект на Django</h1>")
+
+def data(request):
+    return HttpResponse("<h1>Это страница с данными</h1>")
+def test(request):
+    return HttpResponse("<h1>Это тестовая страница с данными</h1>")
